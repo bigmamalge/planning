040e4e144bf4c9e3aeda7111555a3ed43714395e
Malone <bigmamalgemine@gmail.com>
2026-05-08 01:04:18 +0200

    please le dernier commit :(

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.example.planning {
     requires javafx.controls;
     requires javafx.fxml;
 
-    // Ajoute ces lignes pour les outils mobiles
+    // Autorisations pour la création de l'APK
     requires com.gluonhq.attach.display;
     requires com.gluonhq.attach.lifecycle;
     requires com.gluonhq.attach.statusbar;
