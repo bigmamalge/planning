474c0764ec2950261ea1a53abf13f34be29df8f1
Malone <bigmamalgemine@gmail.com>
2026-05-07 23:40:14 +0200

    please le dernier commit :(

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,12 @@ module org.example.planning {
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Ajoute ces lignes pour les outils mobiles
+    requires com.gluonhq.attach.display;
+    requires com.gluonhq.attach.lifecycle;
+    requires com.gluonhq.attach.statusbar;
+    requires com.gluonhq.attach.storage;
+    requires com.gluonhq.attach.util;
 
     opens org.example.planning to javafx.fxml;
     exports org.example.planning;
--- src/main/java/org/example/planning/Launcher.java
+++ src/main/java/org/example/planning/Launcher.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-
-        HelloApplication.main(args);
-
+        Application.launch(HelloApplication.class, args);
     }
 }
