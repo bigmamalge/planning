73d6dfe582e370e3a62292ae5600b82ab221e6b9
Malone <bigmamalgemine@gmail.com>
2026-05-08 13:33:10 +0200

    please le dernier commit :(

--- src/main/java/org/example/planning/HelloController.java
+++ src/main/java/org/example/planning/HelloController.java
@@ -2,8 +2,15 @@ package org.example.planning;
 
 
 import javafx.fxml.FXML;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
+    @FXML
+    TextArea textArea;
 
+
+    @FXML
+    private void initialize(){
+        textArea.setText("Hello World!");
+    }
 }
